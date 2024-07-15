a305f42aa0305d39cf24515ecab85fe4cbc765fa
YoungjuKim <fjvbn2003@gmail.com>
2024-07-15 18:56:22 +0900

    UPDAT bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,13 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 env_path = Path(".") / '.env'
 load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 # print(os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel="#slack-bot-test", text="Hello World!")
